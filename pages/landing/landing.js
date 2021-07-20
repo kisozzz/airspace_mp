32c7cc6eb65f0326cd5be408c444a598bb834e84
Matthew Wu <mnfwu12@gmail.com>
2021-07-20 16:24:46 +0800

    added scroll function to index page, modify card-space-image component

--- pages/landing/landing.js
+++ pages/landing/landing.js
@@ -1,66 +1,101 @@
 // pages/landing/landing.js
-Page({
-
+// Page({
+  Page({
+    data: {
+      cards: [
+        'hold',
+        'hold',
+        'hold',
+        'hold',
+        'hold',
+        'hold'
+      ],
+      indicatorDots: true,
+      autoplay: false,
+      interval: 5000,
+      duration: 1000,
+    },
+    changeIndicatorDots: function(e) {
+      this.setData({
+        indicatorDots: !this.data.indicatorDots
+      })
+    },
+    changeAutoplay: function(e) {
+      this.setData({
+        autoplay: !this.data.autoplay
+      })
+    },
+    intervalChange: function(e) {
+      this.setData({
+        interval: e.detail.value
+      })
+    },
+    durationChange: function(e) {
+      this.setData({
+        duration: e.detail.value
+      })
+    }
+  })
   /**
    * Page initial data
    */
-  data: {
+  // data: {
 
-  },
+  // },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
+//   onLoad: function (options) {
 
-  },
+//   },
 
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
+//   /**
+//    * Lifecycle function--Called when page is initially rendered
+//    */
+//   onReady: function () {
 
-  },
+//   },
 
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
+//   /**
+//    * Lifecycle function--Called when page show
+//    */
+//   onShow: function () {
 
-  },
+//   },
 
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
+//   /**
+//    * Lifecycle function--Called when page hide
+//    */
+//   onHide: function () {
 
-  },
+//   },
 
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
+//   /**
+//    * Lifecycle function--Called when page unload
+//    */
+//   onUnload: function () {
 
-  },
+//   },
 
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
+//   /**
+//    * Page event handler function--Called when user drop down
+//    */
+//   onPullDownRefresh: function () {
 
-  },
+//   },
 
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
+//   /**
+//    * Called when page reach bottom
+//    */
+//   onReachBottom: function () {
 
-  },
+//   },
 
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
+//   /**
+//    * Called when user click on the top right corner to share
+//    */
+//   onShareAppMessage: function () {
 
-  }
-})
+//   }
+// })
