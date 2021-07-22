c5330e4c7cc6646ad31bb3b31d29c3da125fd68e
Matthew Wu <mnfwu12@gmail.com>
2021-07-22 13:59:23 +0800

    modify for login

--- app.js
+++ app.js
@@ -5,20 +5,21 @@ App({
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-    const that = this
     // 登录
+    const that = this
     wx.login({
       success: res => {
         console.log(res)
         wx.request({
-          // to do: replace with heroku url
-          url: 'http://localhost:3000/api/v1/login',
+          // http://localhost:3000/api/v1/login
+          // https://airspace-api.herokuapp.com/api/v1/login
+          url: 'https://airspace-api.herokuapp.com/api/v1/login',
           method: 'POST',
           data: {
             code: res.code
           },
           success(res) {
-            // console.log(res)
+            console.log(res)
             that.globalData.user = res.data.user
           }
         })
