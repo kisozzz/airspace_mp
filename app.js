85ab767c8b0d50f98fb7364552058dac564e77bb
Matthew Wu <mnfwu12@gmail.com>
2021-07-22 11:52:21 +0800

    login functions

--- app.js
+++ app.js
@@ -5,15 +5,27 @@ App({
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    const that = this
     // 登录
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res)
+        wx.request({
+          // to do: replace with heroku url
+          url: 'http://localhost:3000/api/v1/login',
+          method: 'POST',
+          data: {
+            code: res.code
+          },
+          success(res) {
+            // console.log(res)
+            that.globalData.user = res.data.user
+          }
+        })
       }
     })
   },
   globalData: {
-    userInfo: null
+   user: null 
   }
 })
