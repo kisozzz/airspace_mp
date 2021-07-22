514cd47907eb9b0fe3a1f5022b04a2ecd2a85aa0
Dylan Wang <wangvc2017@gmail.com>
2021-07-22 12:11:19 +0800

    added review card component and displays reviews on space page

---
+++ components/review-card/review-card.js
@@ -0,0 +1,23 @@
+// components/review-card.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    review: Object,
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
